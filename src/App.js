0bf2409c1ef1c1153f7944a536f230337af3d5a1
William <willrodrigues4@gmail.com>
2021-08-29 19:06:27 -0300

    Create e Delete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import React from 'react'
 import Login from '../src/pages/Login'
 import Cadastro from '../src/pages/Cadastro'
+import Listagem from '../src/pages/Listagem'
 
 function App() {
   return (
     <div className="App">
-      <Cadastro />
+      <Listagem />
     </div>
   );
 }
