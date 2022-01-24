11ecdaef56f03d626d0dd682af1ceee8ed943709
William <willrodriguess77@gmail.com>
2022-01-24 16:28:17 -0300

    Estrutura do navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,245 @@
+import * as React from 'react';
+import { styled, useTheme, alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+
+const drawerWidth = 240;
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+}));
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+
+
+const menuId = 'primary-search-account-menu';
+
+export default function Navbar() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Pesquisar"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Nova Consulta', 'Minhas Consultas', 'Histórico', 'Alterar Cadastro'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        
+      </Main>
+    </Box>
+  );
+}
--- src/pages/UsuariosListagem/index.js
+++ src/pages/UsuariosListagem/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import api from '../../services/api'
 import { makeStyles } from '@mui/styles'
 import { Button, ButtonGroup, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow }from '@mui/material'
+import Navbar from '../../components/Navbar'
 
 function UsuariosListagem(){
     
@@ -30,45 +31,47 @@ function UsuariosListagem(){
     }
 
     return (
-        <Paper>
-            <h2>Listagem de Usuários</h2>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>NOME</TableCell>
-                        <TableCell>DATA DE NASCIMENTO</TableCell>
-                        <TableCell>GÊNERO</TableCell>
-                        <TableCell>E-MAIL</TableCell>
-                        <TableCell>CRIADO EM</TableCell>
-                        <TableCell>ATUALIZADO EM</TableCell>
-                        <TableCell>AÇÕES</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map(u => (
-                        <TableRow key={u.id}>
-                            <TableCell component="th" scope="row">{u.id}</TableCell>
-                            <TableCell component="th" scope="row">{u.nome}</TableCell>
-                            <TableCell>{new Date(u.dt_nascimento).toLocaleDateString('pt-br')}</TableCell>
-                            <TableCell>{u.genero}</TableCell>
-                            <TableCell>{u.email}</TableCell>
-                            <TableCell>{new Date(u.created_at).toLocaleString('pt-br')}</TableCell>
-                            <TableCell>{new Date(u.updated_at).toLocaleString('pt-br')}</TableCell>
-                            <TableCell>
-                                <ButtonGroup aria-label="outlined primary button group">
-                                    <Button color="primary" href={`/usuario/editar/${u.id}`}>Atualizar</Button>
-                                    <Button color="secondary" onClick={() => remove(u.id)}>Excluir</Button>
-                                </ButtonGroup>
-                            </TableCell>
+        <>
+            <Navbar/>
+            <Paper>
+                <h2>Listagem de Usuários</h2>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>NOME</TableCell>
+                            <TableCell>DATA DE NASCIMENTO</TableCell>
+                            <TableCell>GÊNERO</TableCell>
+                            <TableCell>E-MAIL</TableCell>
+                            <TableCell>CRIADO EM</TableCell>
+                            <TableCell>ATUALIZADO EM</TableCell>
+                            <TableCell>AÇÕES</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
-
+                    </TableHead>
+                    <TableBody>
+                        {users.map(u => (
+                            <TableRow key={u.id}>
+                                <TableCell component="th" scope="row">{u.id}</TableCell>
+                                <TableCell component="th" scope="row">{u.nome}</TableCell>
+                                <TableCell>{new Date(u.dt_nascimento).toLocaleDateString('pt-br')}</TableCell>
+                                <TableCell>{u.genero}</TableCell>
+                                <TableCell>{u.email}</TableCell>
+                                <TableCell>{new Date(u.created_at).toLocaleString('pt-br')}</TableCell>
+                                <TableCell>{new Date(u.updated_at).toLocaleString('pt-br')}</TableCell>
+                                <TableCell>
+                                    <ButtonGroup aria-label="outlined primary button group">
+                                        <Button color="primary" href={`/usuario/editar/${u.id}`}>Atualizar</Button>
+                                        <Button color="secondary" onClick={() => remove(u.id)}>Excluir</Button>
+                                    </ButtonGroup>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </>
     )
 }
 
