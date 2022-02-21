43e4823b835b60a3158ea4c73adcd64fe6918fb6
William <willrodriguess77@gmail.com>
2022-02-21 15:18:27 -0300

    Criando sessao no navegador para salvar token

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Button, Grid, InputAdornment, TextField } from '@mui/material'
-import { Email, Https, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Email, Https, LocalConvenienceStoreOutlined, Visibility, VisibilityOff } from '@mui/icons-material'
 import logo from "../../img/logoAzulHoriz.png"
 import photo from "../../img/photo_login.png"
 import api from '../../services/api'
@@ -51,13 +51,25 @@ function Login() {
 
     async function handleLogin() {
         try {
-            const res = await api.post("/login", { email: email, senha: senha });
-            // console.log(res.data);
-            const usuario = res.data.usuario
+            if(!email){
+                alert("Campo e-mail é obrigatório!")
+                return
+            }
+            
+            if(!senha){
+                alert("Campo senha é obrigatório!")
+                return
+            }
 
-            if(!usuario){
+            const res = await api.post("/login", { email: email, senha: senha });
+
+            const usuario = res.data
+
+            if(!usuario.accessToken){
                 alert("E-mail e/ou senha incorreto(s)!")
+                return
             }else{
+                localStorage.setItem("token", usuario.accessToken)
                 setLoading(false);
                 history.push('/index');
             }
@@ -79,14 +91,6 @@ function Login() {
             console.error("ops! ocorreu um erro" + err)
         }
     }
-
-    // function loadLogin() {
-    //     setLoading(true);
-    //     setTimeout(
-    //        () => handleLogin(),
-    //        2000
-    //     )
-    // }
 
     const handleChangeEyeIcon = (e) => {
         e.preventDefault()
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: "http://localhost:3333"
 });
 
+api.interceptors.request.use(async config => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    api.defaults.headers.authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+
 export default api;
