57e80202afead66232a2a1b480a72a7ae44ac14a
William <willrodriguess77@gmail.com>
2022-02-27 20:01:44 -0300

    Task configuracoes

---
+++ src/components/Configuracoes/FormUsuario.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from '@mui/material'
+
+import { Box, FormControl, InputAdornment, InputLabel, MenuItem, Select } from '@mui/material'
+import {AccountBox, Email, Lock, LocalPhone} from '@mui/icons-material'
+import api from '../../services/api'
+import {DoubleItem, InputItem} from '../../styles/Cadastro'
+
+function FormUsuario() {
+    const [nome, setNome] = useState("")
+    const [genero, setGenero] = useState("")
+    const [email, setEmail] = useState("")
+    const [telefone, setTelefone] = useState("")
+    const [senha, setSenha] = useState("")
+
+    useEffect(() => {
+        getUsuario();
+    },[])
+
+    async function getUsuario(){
+        const result = await api.get('/usuario')
+        setNome(result.data.nome)
+        setGenero(result.data.genero)
+        setEmail(result.data.email)
+        setTelefone(result.data.telefone)     
+    }
+
+    async function atualizarDadosUsuario(){
+        if(!nome){
+            alert('Nome é obrigatório!')
+            return
+        }
+
+        if(!genero){
+            alert('Genero é obrigatório!')
+            return
+        }
+
+        if(!email){
+            alert('E-mail é obrigatório!')
+            return
+        }
+
+        if(!telefone){
+            alert('Telefone é obrigatório!')
+            return
+        }
+
+        if(senha){
+            await api.put('/usuario/editar', {data:{nome, genero, email, telefone, senha}})
+        }else{
+            await api.put('/usuario/editar', {data:{nome, genero, email, telefone}})
+        }
+        alert('Dados de usuário atualizado com sucesso!')
+        window.location.reload(false)
+        
+    }
+
+    return (
+        <>
+            <form>
+                <DoubleItem>
+                    <Box>
+                        <InputItem
+                            style={{ width: '100%' }}
+                            variant="filled"
+                            label="Nome"
+                            value={nome}
+                            onChange={e => setNome(e.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <AccountBox />
+                                    </InputAdornment>),
+                            }}
+                        />
+                    </Box>
+                    <Box>
+                        <FormControl variant="filled">
+                            <InputLabel>Gênero</InputLabel>
+                            <Select
+                                style={{ width: 208 }}
+                                value={genero}
+                                onChange={e => setGenero(e.target.value)}
+                            >
+                                <MenuItem value="H">Homem</MenuItem>
+                                <MenuItem value="M">Mulher</MenuItem>
+                                <MenuItem value="O">Outro</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </DoubleItem>
+                <DoubleItem>
+                    <Box>
+                        <InputItem
+                            style={{ width: '100%' }}
+                            type="tel"
+                            variant="filled"
+                            label="Telefone"
+                            value={telefone}
+                            onChange={e => setTelefone(e.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <LocalPhone />
+                                    </InputAdornment>),
+                            }}
+                        />
+                    </Box>
+                    <Box>
+                        <InputItem
+                            style={{ width: '100%' }}
+                            type="email"
+                            variant="filled"
+                            label="E-mail"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <Email />
+                                    </InputAdornment>),
+                            }}
+                        />
+                    </Box>
+                </DoubleItem>
+
+                <DoubleItem>
+                    <Box>
+                        <InputItem
+                            style={{ width: '100%' }}
+                            type="password"
+                            variant="filled"
+                            label="Senha"
+                            onChange={e => setSenha(e.target.value)}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <Lock />
+                                    </InputAdornment>),
+                            }}
+                        />
+                    </Box>
+                    
+                </DoubleItem>
+                <Button variant="contained" size="large" color="success" sx={{ margin: 1 }} onClick={atualizarDadosUsuario}><h4>ATUALIZAR</h4></Button>
+
+            </form>
+        </>
+    )
+}
+
+export default FormUsuario
---
+++ src/pages/Consultas/index.js
@@ -0,0 +1,16 @@
+function Consultas(){
+    return(
+        <>
+            <ul>                
+                    <ol>
+                    <li>  1) Visualizar Consultas (agenda)</li>
+                    <li>  2) Visualizar uma consulta</li>
+                    <li>  3) Alterar Consulta</li>
+                    <li>  4) Cancelar Consulta</li>
+                </ol>
+            </ul>
+        </>
+    )
+}
+
+export default Consultas
